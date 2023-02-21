2d1ce354ea31bc27dd3921a5d9bc2245c933be9a
chicken nugget <116309083+chickennu99et@users.noreply.github.com>
2023-02-21 16:54:30 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var num = 0;
+var change = 0;
+function add()
+{
+  num = num + change;
+}
+function minus()
+{
+  num = num - change;
+}
+function clear()
+{
+  num = num - num;
+}
+function divide()
+{
+  num = num/change;
+}
+function multiply
+{
+  num = num*change;
+}
